60780cf3ac09eacc19c74ce0e61a7d0a7f602ed9
MSandru25 <msandruwork@gmail.com>
2024-02-28 20:51:35 +0200

    Added "Piramida"

---
+++ Piramida/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Introduceti un numar");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(i);
+    }
+    Console.WriteLine();
+}
