65b0489f7509b164b3f1d4d3e48888d3736eebf1
MSandru25 <msandruwork@gmail.com>
2024-02-28 11:03:01 +0200

    optimized

--- FTIT L2 E5/Program.cs
+++ FTIT L2 E5/Program.cs
@@ -1,4 +1,4 @@
-﻿List<int> myList = new List<int>();
+﻿/*List<int> myList = new List<int>();
 Console.WriteLine("Cate numere are sirul ?");
 int myListCount = int.Parse(Console.ReadLine());
 for (int i = 0; i < myListCount; i++)
@@ -18,9 +18,33 @@ foreach (int i in myList)
         sum += i;
     }
 }
-Console.WriteLine($"Media aritmetica a numerelor pare este {sum / count}");
+Console.WriteLine($"Media aritmetica a numerelor pare este {sum / count}");*/
+
+
+Console.Write("Introduceti numărul de elemente din sir: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Introduceti numarul {i + 1}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+        sum += num;
+        count++;
+    }
+}
+if (count == 0)
+{
+    count = 1;
+}
+
+Console.WriteLine($"Media aritmetica a numerelor pare este: {sum / count}");
 
 /*Ex 5: Să se scrie un program care citeşte de la tastatură un şir de n numere
 naturale şi determină media aritmetică a celor pare, n fiind citit de la
 tastatra*/
-// Nu am stiu sa fac cu un numar de cifre nespecificat inainte in lista. Poate ma lamuriti :).
+// Nu am stiut sa fac cu un numar de cifre nespecificat inainte in lista. Poate ma lamuriti :).
